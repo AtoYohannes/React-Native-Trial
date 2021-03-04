34ad0031657f8db17a42d59f0b8086eba3723e13
Yohannes Berhanu <Johnlights51@gmail.com>
2021-03-04 19:09:48 +0300

    Started Learning React-Native

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { styles } from "../styles/tabTwo";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -14,19 +14,3 @@ export default function TabTwoScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
